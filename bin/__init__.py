0a065f2fd1fdda20c7735a7f9a31845272a9188f
Bapan Sardar <sbapan@quantumhash.me>
2026-01-23 21:03:00 +0530

    Initial commit: qhash-python

---
+++ bin/__init__.py
@@ -0,0 +1,3 @@
+"""
+author: bapan.sardar
+"""
---
+++ bin/artifacts-generate.sh
@@ -0,0 +1,76 @@
+python3 -m grpc.tools.protoc \
+    -I ./qhash/clients/protos/artifacts \
+    --pyi_out=./qhash/clients/protos \
+    --python_out=./qhash/clients/protos \
+    --grpc_python_out=./qhash/clients/protos \
+    ./qhash/clients/protos/artifacts/*.proto
+
+
+
+# for file in qhash/clients/protos/*.py; do
+#     if grep -q "import assistant_.*_pb2" "$file"; then
+#         sed 's/import \(assistant_.*_pb2\)/import qhash.clients.protos.\1/g' "$file" > "${file}.tmp" && mv "${file}.tmp" "$file"
+#     fi
+# done
+for file in qhash/clients/protos/*.py qhash/clients/protos/*.pyi; do
+
+# import document_api_pb2 as document__api__pb2
+
+     if grep -q "import .*_api_pb2 " "$file"; then
+        sed 's/import \(.*_api_pb2\)/import qhash.clients.protos.\1/g' "$file" > "${file}.tmp" && mv "${file}.tmp" "$file"
+    fi
+
+    # Handle assistant_*_pb2 imports
+    if grep -q "import assistant_.*_pb2" "$file"; then
+        sed 's/import \(assistant_.*_pb2\)/import qhash.clients.protos.\1/g' "$file" > "${file}.tmp" && mv "${file}.tmp" "$file"
+    fi
+
+    # Handle common_pb2 imports
+    if grep -q "import common_pb2" "$file"; then
+        sed 's/import common_pb2 as common__pb2/import qhash.clients.protos.common_pb2 as common__pb2/g' "$file" > "${file}.tmp" && mv "${file}.tmp" "$file"
+        sed 's/import common_pb2 as _common_pb2/import qhash.clients.protos.common_pb2 as _common_pb2/g' "$file" > "${file}.tmp" && mv "${file}.tmp" "$file"
+    fi
+
+    # Handle web_api_pb2 imports
+    if grep -q "import web_api_pb2" "$file"; then
+        sed 's/import web_api_pb2 as web__api__pb2/import qhash.clients.protos.web_api_pb2 as web__api__pb2/g' "$file" > "${file}.tmp" && mv "${file}.tmp" "$file"
+    fi
+
+    # Handle provider_api_pb2 imports
+    if grep -q "import provider_api_pb2" "$file"; then
+        sed 's/import provider_api_pb2 as provider__api__pb2/import qhash.clients.protos.provider_api_pb2 as provider__api__pb2/g' "$file" > "${file}.tmp" && mv "${file}.tmp" "$file"
+    fi
+
+    # Handle vault_api_pb2 imports
+    if grep -q "import vault_api_pb2" "$file"; then
+        sed 's/import vault_api_pb2 as vault__api__pb2/import qhash.clients.protos.vault_api_pb2 as vault__api__pb2/g' "$file" > "${file}.tmp" && mv "${file}.tmp" "$file"
+    fi
+
+    # Handle integration_api_pb2 imports
+    if grep -q "import integration_api_pb2" "$file"; then
+        sed 's/import integration_api_pb2 as integration__api__pb2/import qhash.clients.protos.integration_api_pb2 as integration__api__pb2/g' "$file" > "${file}.tmp" && mv "${file}.tmp" "$file"
+    fi
+
+    # Handle knowledge_api_pb2 imports
+    if grep -q "import knowledge_api_pb2" "$file"; then
+        sed 's/import knowledge_api_pb2 as knowledge__api__pb2/import qhash.clients.protos.knowledge_api_pb2 as knowledge__api__pb2/g' "$file" > "${file}.tmp" && mv "${file}.tmp" "$file"
+    fi
+
+    # Handle invoker_api_pb2 imports
+    if grep -q "import invoker_api_pb2" "$file"; then
+        sed 's/import invoker_api_pb2 as invoker__api__pb2/import qhash.clients.protos.invoker_api_pb2 as invoker__api__pb2/g' "$file" > "${file}.tmp" && mv "${file}.tmp" "$file"
+    fi
+
+    # Handle assistant_deployment_pb2 imports
+    if grep -q "import assistant_deployment_pb2" "$file"; then
+        sed 's/import assistant_deployment_pb2 as assistant__deployment__pb2/import qhash.clients.protos.assistant_deployment_pb2 as assistant__deployment__pb2/g' "$file" > "${file}.tmp" && mv "${file}.tmp" "$file"
+    fi
+
+    # Handle talk_api_pb2 imports
+    if grep -q "import talk_api_pb2" "$file"; then
+        sed 's/import talk_api_pb2 as talk__api__pb2/import qhash.clients.protos.talk_api_pb2 as talk__api__pb2/g' "$file" > "${file}.tmp" && mv "${file}.tmp" "$file"
+    fi
+done
+
+# Remove backup files created by sed
+find "qhash/clients/protos/" -name '*.bak' -exec rm {} +
